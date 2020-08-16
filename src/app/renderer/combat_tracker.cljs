2eda0d61f6f1eae23a4cde15a92ffab40fccf7a2
Allison Casey <alliecasey21@gmail.com>
2020-08-16 00:52:26 -0400

    combat tracker mvp

--- src/app/renderer/combat_tracker.cljs
+++ src/app/renderer/combat_tracker.cljs
@@ -6,73 +6,9 @@
             [app.renderer.events :as events]
             [goog.string :as gstring]
             [goog.string.format]
+            [cuerdas.core :as cuerdas]
             [app.renderer.macros :refer [when-let*]]))
 
-
-(defn ability-li [selected? char-id {:keys [id name description cooldown] :as ability}]
-  [:li
-   {:class [(when selected? "bg-info")]}
-   [:div
-    {:on-mouse-enter #(rf/dispatch [:set-highlighted-ability char-id id])
-     :on-mouse-leave #(rf/dispatch [:remove-highlighted-ability])}
-    [button
-     :label "Use"
-     :disabled? @(rf/subscribe [::subs/ability-on-cooldown? char-id id])
-     :on-click #(rf/dispatch [:use-ability char-id ability])]
-    (gstring/format "Name: %s, Description: %s, Cooldown: %d" name description cooldown)]])
-
-(defn character-info []
-  (let [character @(rf/subscribe [::subs/selected-character])
-        {char-id :character-id ability-id :ability-id} @(rf/subscribe [::subs/highlighted-ability])]
-    (when-let [{:keys [name description abilities passives]} character]
-      [:div
-       [:h4 "Character Info"]
-       [:h5 "Name:" name]
-       [:div "Description: " description]
-       [:h6 "Passives:"]
-       [:ul
-        (for [{:keys [id name description]} passives]
-          ^{:key id}
-          [:li name ": " description])]
-       [:h6 "Abilities:"]
-       [:ul
-        (doall (for [[id ability] abilities]
-                 ^{:key id}
-                 [ability-li
-                  (and (= char-id (:uuid character)) (= ability-id (:id ability)))
-                  (:uuid character)
-                  ability]))]])))
-
-(defn active-characters []
-  (let [characters @(rf/subscribe [::subs/characters])
-        active-character @(rf/subscribe [::subs/selected-character-id])]
-    [:div
-     [:h4 "Characters"]
-     [:ul.list-group
-      (for [{:keys [uuid name]} characters]
-        ^{:key uuid}
-        [:li
-         {:on-click #(rf/dispatch [:set-selected-character-id uuid])
-          :class ["list-group-item" (if (= uuid active-character) "active")]}
-         name])]]))
-
-(defn ability-timeline []
-  (let [on-cooldown @(rf/subscribe [::subs/abilities-on-cooldown])
-        {:keys [character-id ability-id]} @(rf/subscribe [::subs/highlighted-ability])]
-    [:div
-     [:h4 "Ability Timeline"]
-     (for [[round chunk] on-cooldown]
-       ^{:key round}
-       [:div
-        [:h5 "Round: " round]
-        [:ul
-         (for [[char-id {:keys [id name]}] chunk]
-           ^{:key id}
-           [:li
-            {:class [(when (and (= char-id character-id) (= ability-id id)) "bg-info")]}
-            name])]])]))
-
-;;; Refactored
 (defn character-selector []
   (let [characters @(rf/subscribe [::subs/characters])
         active-character @(rf/subscribe [::subs/selected-character-id])]
@@ -88,51 +24,92 @@
             :class ["list-group-item" (if (= uuid active-character) "active")]}
            name])]]]]))
 
-(defn character []
-  (letfn [(ability-li [char-id {:keys [id name description cooldown back-in] :as ability}]
-            [:a.list-group-item.list-group-item-action.flex-column.align-items-start
-             {:on-click (fn [event]
-                          (rf/dispatch [:use-ability char-id ability])
-                          (.preventDefault event))
-              :on-mouse-enter #(rf/dispatch [:set-highlighted-ability char-id id])
-              :on-mouse-leave #(rf/dispatch [:remove-highlighted-ability])}
-             [:div.d-flex.w-100.justify-content-between
-              [:h5.mb-1 name]
-              (when-not (zero? (or back-in 0)) [:span.badge.badge-primary.badge-pill back-in])]
-             [:p.mb-1description description]
-             [:p [:strong "Cooldown"] " : " cooldown]])
-          (passive-li [{:keys [name description]}]
-            [:div.list-group-item.list-group-item-action.flex-column.align-items-start
-             {:href "#"}
-             [:div.d-flex.w-100
-              [:h5.mb-1 name]]
-             [:p.mb-1description description]])]
-    (when-let* [{:keys [uuid name faction description abilities passives] :as x} @(rf/subscribe [::subs/selected-character])
-                ;; {char-id :character-id ability-id :ability-id} @(rf/subscribe [::subs/highlighted-ability])
-                ]
-      (let [abilities-lis (for [ability (vals abilities)]
-                            ^{:key (:id ability)}
-                            (ability-li uuid ability))
-            passives-lis (for [passive passives]
-                           ^{:key (:id passives)}
-                           (passive-li passive))]
-        [:<>
-         [:h4 "Character Info"]
+(defn accordion [title & {:keys [children left right]
+                          :or {left [] right []}}]
+  (let [s (r/atom {:open? false
+                   :child-height 0})]
+    (fn [title & {:keys [children left right]
+                  :or {left [] right []}}]
+      (let [{:keys [open? child-height]} @s]
+        [:div.accordion
          [:div.card
-          [:div.card-body
-           [:h5.card-title name]
-           [:h6.card-subtitle.text-muted faction]
-           [:p.card-text description]
-           (when (seq passives-lis)
-             [:<>
-              [:hr]
-              [:h6 "Passives"]
-              [:div.list-group.list-group-flush passives-lis]])
-           (when (seq abilities-lis)
-             [:<>
-              [:hr]
-              [:h6 "Abilities"]
-              [:div.list-group.list-group-flush abilities-lis]])]]]))))
+          [:div.card-header
+           [:div.d-flex.w-100 ;;.justify-content-between
+            (when (seq left) (into [:div] left))
+            [:div.flex-grow-1.ml-1.align-middle.mb-0 title]
+            (when (seq right) (into [:div] right))
+            [:a.ml-1.align-middle
+             {:on-click #(swap! s update :open? not)
+              :style {:cursor "pointer"}}
+             [:i.fa {:class [(if open? "fa-minus" "fa-plus")]}]]]]
+          [:div.collapse.show
+           {:style {:max-height (if open? child-height 0)
+                    :transition "max-height 0.8s"
+                    :overflow "hidden"}}
+           (into [:div.card-body
+                  {:ref #(when % (swap! s assoc :child-height (.-clientHeight %)))}]
+                 children)]]]))))
+
+(defn kv [key value]
+  [:p.m-0 [:strong key] " : " value])
+
+(defn ^:private ability-li
+  [char-id {:keys [id name description cooldown back-in] :as ability}]
+  (let [on-cooldown? (zero? (or back-in 0))]
+    [:div.list-group-item.list-group-item-action.flex-column.align-items-start
+     {:on-mouse-enter #(rf/dispatch [:set-highlighted-ability char-id id])
+      :on-mouse-leave #(rf/dispatch [:remove-highlighted-ability])
+      :style {:padding 0
+              :margin-bottom "0.25em"}}
+     [accordion name
+      :left [[:i.fa.fa-fw {:style {:cursor (if on-cooldown? "pointer" "not-allowed")}
+                           :class [(if on-cooldown? "fa-check-circle-o" "fa-times-circle-o")]
+                           :on-click (fn [event]
+                                       (rf/dispatch [:use-ability char-id ability])
+                                       (.preventDefault event))}]]
+      :right [(when-not on-cooldown? [:span.badge.badge-primary.badge-pill back-in])]
+      :children [[:p.mb-1description description]
+                 [kv "Cooldown" cooldown]]]]))
+
+(defn ^:private passive-li
+  [{:keys [name description]}]
+  [:div.list-group-item.list-group-item-action.flex-column.align-items-start
+   {:style {:padding 0
+            :margin-bottom "0.25em"}}
+   [accordion name
+    :children [[:p.mb-1description description]]]])
+
+(defn character []
+  (when-let* [{:keys [uuid name faction description abilities passives health dt] :as x} @(rf/subscribe [::subs/selected-character])
+              ;; {char-id :character-id ability-id :ability-id} @(rf/subscribe [::subs/highlighted-ability])
+              ]
+    (let [abilities-lis (for [ability (vals abilities)]
+                          ^{:key (:id ability)}
+                          (ability-li uuid ability))
+          passives-lis (for [passive passives]
+                         ^{:key (:id passives)}
+                         (passive-li passive))]
+      [:<>
+       [:h4 "Character Info"]
+       [:div.card
+        [:div.card-body
+         [:h5.card-title name]
+         [:h6.card-subtitle.text-muted.mb-2
+          (cuerdas/phrase faction)]
+         [:p.card-text description]
+         [:ul.list-unstyled
+          [:li [kv "Health" health]]
+          [:li [kv "DT" dt]]]
+         (when (seq passives-lis)
+           [:<>
+            [:hr]
+            [:h6 "Passives"]
+            [:div.list-group.list-group-flush passives-lis]])
+         (when (seq abilities-lis)
+           [:<>
+            [:hr]
+            [:h6 "Abilities"]
+            [:div.list-group.list-group-flush abilities-lis]])]]])))
 
 (defn timeline []
   (let [on-cooldown @(rf/subscribe [::subs/abilities-on-cooldown])
@@ -160,14 +137,25 @@
         (for [[round chunk] on-cooldown]
           ^{:key round}
           [:div
-           [:h6.text-center "--- " round " ---"]
+           [:div.d-flex.w-100.justify-content-center
+            [:div.pr-2
+             [:svg {:width "40px" :height "13px" :style {:stroke-opacity 0.5}}
+              [:line {:style {:stroke "black" :stroke-width 1} :x1 0 :x2 35 :y1 6 :y2 6}]
+              [:circle {:style {:stroke "black" :stroke-width 1 :fill "none"}
+                        :cx (+ 35 4) :cy 6 :r 5} ]]]
+            [:div.text-center round]
+            [:div.pl-2
+             [:svg {:width "40px" :height "13px" :style {:stroke-opacity 0.5}}
+              [:line {:style {:stroke "black" :stroke-width 1} :x1 4 :x2 35 :y1 6 :y2 6}]
+              [:circle {:style {:stroke "black" :stroke-width 1 :fill "none"}
+                        :cx 0 :cy 6 :r 5} ]]]]
            [:div.list-group.list-group-flush
             (for [[char-id {:keys [id name]}] chunk]
               ^{:key id}
               [:a.list-group-item.list-group-item-action.flex-column.align-items-start
                {:class [(when (and (= char-id character-id) (= ability-id id)) "bg-info")]
                 :style {"padding" "0.1em"}}
-               [:div.d-flex.w-100.justify-content-between
+               [:div.d-flex.w-100.justify-content-center
                 [:p.mb-1 name]]])]])]]]]))
 
 (defn render []
@@ -177,4 +165,5 @@
    [:div.col-6
     [character]]
    [:div.col
-    [timeline]]])
+    [timeline]]]
+  )
